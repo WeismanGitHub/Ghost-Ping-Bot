574967627b6800f76742cc09389353a17d24a119
weisman <weisman123@outlook.com>
2024-10-19 00:49:55 -0700

    Finish code to run bot/db

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,44 @@
-import { Collection, EmbedBuilder, Events, GatewayIntentBits } from 'discord.js';
-import CustomError from './custom-error';
+import sequelize from './db/sequelize';
 import { readdirSync } from 'fs';
 import { join } from 'path';
+import dotenv from 'dotenv';
+import {
+    Client,
+    ClientOptions,
+    Collection,
+    EmbedBuilder,
+    Events,
+    GatewayIntentBits,
+} from 'discord.js';
 
-const foldersPath = join(__dirname, 'commands');
-const commandFolders = readdirSync(foldersPath);
+class CustomClient extends Client {
+    commands: Collection<unknown, unknown>;
+
+    constructor(options: ClientOptions) {
+        super(options);
+
+        this.commands = new Collection();
+    }
+}
 
 const client = new CustomClient({
     intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds],
 });
 
-client.commands = new Collection();
+const commandsPath = join(__dirname, 'commands');
+const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
 
-for (const folder of commandFolders) {
-    const commandsPath = join(foldersPath, folder);
-    const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-        const filePath = join(commandsPath, file);
-        const command = require(filePath);
-
-        if (!command.data || !command.execute) {
-            throw new Error(
-                `The command at ${filePath} is missing a required "data" or "execute" property.`
-            );
-        }
-
-        client.commands.set(command.data.name, command);
+for (const file of commandFiles) {
+    const filePath = join(commandsPath, file);
+    const command = require(filePath);
+    
+    if (!command.data || !command.execute) {
+        throw new Error(
+            `The command at ${filePath} is missing a required "data" or "execute" property.`
+        );
     }
+
+    client.commands.set(command.data.name, command);
 }
 
 client.on(Events.InteractionCreate, async (interaction) => {
@@ -38,9 +49,14 @@ client.on(Events.InteractionCreate, async (interaction) => {
     const command = client.commands.get(interaction.commandName);
 
     try {
+        // @ts-ignore
         await command.execute(interaction);
     } catch (err) {
+        console.error(err);
+
         const embed = new EmbedBuilder();
+
+        embed.setDescription('error');
 
         if (interaction.replied || interaction.deferred) {
             interaction.followUp({ embeds: [embed], ephemeral: true });
@@ -50,4 +66,17 @@ client.on(Events.InteractionCreate, async (interaction) => {
     }
 });
 
-client.on(Events.GuildMemberAdd, async (member) => {});
+client.on(Events.GuildMemberAdd, async (member) => {
+    console.log(member);
+});
+
+dotenv.configDotenv();
+const token = process.env.TOKEN;
+
+if (!token) {
+    throw new Error('TOKEN is undefined.');
+}
+
+sequelize.authenticate().then(() => {
+    client.login(token).then(() => console.log('> logged in'));
+});
